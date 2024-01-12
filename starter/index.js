e9a357aebdaea89e3517ef9086cf2e52288801c0
eram m juned <eskengg@gmail.com>
2024-01-12 19:46:21 +0000

    added prompts for user to enter details about the project

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,69 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    inquirer
+  .prompt ([
 
+    {
+        type: 'input',
+        message: 'enter the title of your project:',
+        name: 'project-title',
+      },
+      {
+        type: 'input',
+        message: 'sections-entitled:',
+        name: 'project-section',
+      },
+      {
+        type: 'input',
+        message: 'project description:',
+        name: 'project-description',
+      },
+      {
+        type: 'input',
+        message: 'table of contents:',
+        name: 'project-content',
+      },
+      {
+        type: 'input',
+        message: 'installation:',
+        name: 'project-installation',
+      },
+      {
+        type: 'input',
+        message: 'usage:',
+        name: 'project-usage',
+      },
+      {
+        type: 'input',
+        message: 'license:',
+        name: 'project-license',
+      },
+      {
+        type: 'input',
+        message: 'test:',
+        name: 'project-test',
+      },
+      {
+        type: 'input',
+        message: 'questions:',
+        name: 'project-questions',
+      },
+      {
+        type: 'input',
+        message: 'questions:',
+        name: 'project-questions',
+      },
+      {
+        type: 'input',
+        message: 'enter GitHub username:',
+        name: 'project-username',
+      },
+      {
+        type: 'input',
+        message: 'enter GitHub url:',
+        name: 'project-url',
+      },
     ])
 
 ];
