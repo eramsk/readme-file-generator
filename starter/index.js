ef6f6fa5c42752bbd296ba47f7a3e5c2d6aee177
eram m juned <eskengg@gmail.com>
2024-02-16 18:45:42 +0000

    coded read me file generator

--- starter/index.js
+++ starter/index.js
@@ -1,85 +1,86 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer')
+const fs = require('fs')
+const MarkDown = require('./utils/generateMarkdown')
 
-// array of questions for user
+
+// App Questions
+
 const questions = [
-    inquirer
-  .prompt ([
+    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'project title',
+    },
 
     {
         type: 'input',
-        message: 'enter the title of your project:',
-        name: 'project-title',
-      },
-      {
-        type: 'input',
-        message: 'sections-entitled:',
-        name: 'project-section',
-      },
-      {
-        type: 'input',
-        message: 'project description:',
-        name: 'project-description',
-      },
-      {
-        type: 'input',
-        message: 'table of contents:',
-        name: 'project-content',
-      },
-      {
-        type: 'input',
-        message: 'installation:',
-        name: 'project-installation',
-      },
-      {
-        type: 'input',
-        message: 'usage:',
-        name: 'project-usage',
-      },
-      {
-        type: 'input',
-        message: 'license:',
-        name: 'project-license',
-      },
-      {
-        type: 'input',
-        message: 'test:',
-        name: 'project-test',
-      },
-      {
-        type: 'input',
-        message: 'questions:',
-        name: 'project-questions',
-      },
-      {
-        type: 'input',
-        message: 'questions:',
-        name: 'project-questions',
-      },
-      {
-        type: 'input',
-        message: 'enter GitHub username:',
-        name: 'project-username',
-      },
-      {
-        type: 'input',
-        message: 'enter GitHub url:',
-        name: 'project-url',
-      },
-    ])
+        name: 'description',
+        message: 'project description',
+    },
 
-];
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'project usage',
+    },
 
-// function to write README file
-function writeToFile(fileName, data) {
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'contribution info',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How to start',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'for question(email)',
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'for coding(github)',
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License?',
+        choices:['MIT', 'ISC', 'GNUGPLv3'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    },
+
+    
+]
+
+// run query function
+ 
+function runQuery() {
+    return inquirer.prompt(questions)
+    .then((answers)=> {
+        const mark = MarkDown.generateReadme(answers)
+        fs.writeFile('README.md', mark, function(err) {
+    if(err) {
+        console.log('Could not save file', err);
+    } else {
+        console.log('Success: new README.md file generated inside the current folder');
+    }
+    })
+    
+    })
+
+    .catch((error) => { 
+        console.log(error)
+    })
 }
 
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+runQuery()
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,66 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+class MarkDown {
 
-`;
+  static renderLicenseBadge(license){
+      const badges = {
+          isc: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+          mit:'[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)            ',
+          gnugplv3: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_V3-blue.svg)](https://opensource.org/license/cern-ohl-s/)'
+       }
+  return badges[license]
 }
 
-module.exports = generateMarkdown;
+static renderLicenseLink(license) {
+  const licenseLinks = {
+      isc: '[ISC](https://choosealicense.com/licenses/isc/)',
+      mit: '[MIT](https://choosealicense.com/licenses/mit/)',
+      gnugplv3: '[GNUGPLv3](https://choosealicense.com/licenses/gpl-3.0/)'
+  }
+  return licenseLinks[license]
+}
+
+static renderLicenseSection(license){
+  if(license) {
+      return `Licensed under the ${this.renderLicenseLink(license)} license`
+  } else 
+      return ''
+  }
+
+
+static generateReadme(answers){
+      return `
+
+# ${answers.title}
+  
+${this.renderLicenseBadge(answers.license)}
+
+## Table of Content
+- [Project Description](#Description)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Installation](#Installation)
+- [Questions](#Questions)
+- [Lincese](#License)
+
+## Description
+${answers.description}
+
+## Usage
+${answers.usage}
+
+## Contribution 
+${answers.contribution}
+
+## Installation 
+${answers.installation}
+
+## Questions
+${answers.email}
+${answers.github}
+
+## License
+${this.renderLicenseSection(answers.license)}
+  `
+  }
+}
+
+module.exports = MarkDown
